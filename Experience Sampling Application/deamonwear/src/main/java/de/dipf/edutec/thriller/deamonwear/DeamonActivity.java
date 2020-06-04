bb78efce1d32631963a5b91effbaffdd15f0b98f
Sebastian Rödling <sebastian.roedling@outlook.com>
2020-06-04 16:11:30 +0200

    -- Added deamonwear module to project --- change deamonwear to WearableActivity

--- Experience Sampling Application/deamonwear/src/main/java/de/dipf/edutec/thriller/deamonwear/DeamonActivity.java
+++ Experience Sampling Application/deamonwear/src/main/java/de/dipf/edutec/thriller/deamonwear/DeamonActivity.java
@@ -3,12 +3,14 @@ package de.dipf.edutec.thriller.deamonwear;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
 
-public class DeamonActivity extends AppCompatActivity {
+public class DeamonActivity extends WearableActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deamon);
+        setAmbientEnabled();
     }
 }
