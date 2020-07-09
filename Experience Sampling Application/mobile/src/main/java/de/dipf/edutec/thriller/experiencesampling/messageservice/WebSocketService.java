9a7c10ff0f6bf488ad6c941ef736887ef13d4b06
Sebastian Rödling <sebastian.roedling@outlook.com>
2020-07-09 16:22:26 +0200

    -- Added Live Websocket Notifications

--- Experience Sampling Application/mobile/src/main/java/de/dipf/edutec/thriller/experiencesampling/messageservice/WebSocketService.java
+++ Experience Sampling Application/mobile/src/main/java/de/dipf/edutec/thriller/experiencesampling/messageservice/WebSocketService.java
@@ -1,12 +1,18 @@
 package de.dipf.edutec.thriller.experiencesampling.messageservice;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.preference.PreferenceManager;
 
@@ -25,6 +31,7 @@ import okio.ByteString;
 import tech.gusavila92.websocketclient.WebSocketClient;
 
 public class WebSocketService extends Service {
+    public static final String CHANNEL_ID = "ForegroundServiceChannel";
     // Static
     private static String TAG = "WebSockerService: ";
     String PATH_SMARTWATCH_TEST;
@@ -49,6 +56,7 @@ public class WebSocketService extends Service {
         this.intent = intent;
         this.flags = flags;
         this.startid = startid;
+
         PATH_SMARTWATCH_TEST = getResources().getString(R.string.PATH_TOSMARTWATCH_TEST);
         client = new OkHttpClient();
         messagesSingleton = MessagesSingleton.getInstance();
@@ -61,7 +69,36 @@ public class WebSocketService extends Service {
         EchoWebSocketListener listener = new EchoWebSocketListener();
         ws = client.newWebSocket(request, listener);
         //client.dispatcher().executorService().shutdown();
-        return START_STICKY;
+
+
+        String input = intent.getStringExtra("inputExtra");
+        createNotificationChannel();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Foreground Service")
+                .setContentText(input)
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1, notification);
+
+
+
+        return START_NOT_STICKY;
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
     }
 
     void sendMsgTest(final String text){
@@ -118,9 +155,12 @@ public class WebSocketService extends Service {
 
 
 
+
+
     private final class EchoWebSocketListener extends WebSocketListener {
 
         private static final int NORMAL_CLOSURE_STATUS = 1000;
+
 
         @Override
         public void onOpen(WebSocket webSocket, Response response) {
@@ -179,6 +219,8 @@ public class WebSocketService extends Service {
             onStartCommand(intent,flags,startid);
         }
     }
+
+
 
 
 
