f6c40ebc4231bfa17db1a3256e593331aad2bb9a
Felix Jian <felix.d.jian@gmail.com>
2020-08-07 19:00:10 +0200

    generate UUID as client ID

--- Experience Sampling Application/mobile/src/main/java/de/dipf/edutec/thriller/experiencesampling/conf/ApplicationContext.java
+++ Experience Sampling Application/mobile/src/main/java/de/dipf/edutec/thriller/experiencesampling/conf/ApplicationContext.java
@@ -2,6 +2,12 @@ package de.dipf.edutec.thriller.experiencesampling.conf;
 
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.Toast;
+import com.google.android.gms.auth.api.credentials.*;
+import com.google.android.gms.common.api.ResolvableApiException;
 import de.dipf.edutec.thriller.experiencesampling.activities.MainActivity;
 import de.dipf.edutec.thriller.experiencesampling.foreground.ForegroundNotificationCreator;
 import de.dipf.edutec.thriller.experiencesampling.sensorservice.transport.*;
@@ -10,6 +16,9 @@ import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Getter
 public class ApplicationContext {
     // TODO: unsafe certificate validation for development, turn off later
@@ -17,6 +26,8 @@ public class ApplicationContext {
     private static final String CHANNEL_ID = "ForegroundServiceChannel";
     private static final String NOTIFICATION_CONTENT_TITLE = "App is still running";
     private static final String NOTIFICATION_CONTENT_TEXT = "Tap to go back to the app.";
+
+    // TODO: make user configurable
     private static final String MQTT_BROKER_URL = "ssl://flex-pc:8883";
 
     private final MqttService mqttService;
@@ -30,9 +41,70 @@ public class ApplicationContext {
         connOpts.setConnectionTimeout(10);
         SslSocketFactoryFactory sslSocketFactoryFactory = getSslSocketFactoryWrapper(ctx);
 
+        // if null, there was no certificate provided in PROD so the trusted android certificates will be allowed
         if(sslSocketFactoryFactory != null) {
             connOpts.setSocketFactory(sslSocketFactoryFactory.create());
         }
+
+        // TODO: put this right after the successful login to mqtt client with provided credentials
+//        CredentialsClient client = Credentials.getClient(ctx);
+//        Credential cred = new Credential.Builder("username")
+//                .setPassword("password")
+//                .setAccountType("thriller")
+//                .build();
+//
+//        client.save(cred).addOnCompleteListener(task -> {
+//            if (task.isSuccessful()) {
+//                Log.d("tag", "SAVE: OK");
+//                Toast.makeText(ctx, "Credentials saved", Toast.LENGTH_SHORT).show();
+//                return;
+//            }
+//
+//            Exception e = task.getException();
+//            if (e instanceof ResolvableApiException) {
+//                // Try to resolve the save request. This will prompt the user if
+//                // the credential is new.
+//                ResolvableApiException rae = (ResolvableApiException) e;
+//                try {
+//                    rae.startResolutionForResult(activity, 1);
+//                } catch (IntentSender.SendIntentException exception) {
+//                    // Could not resolve the request
+//                    Log.e("tag", "Failed to send resolution.", exception);
+//                    Toast.makeText(ctx, "Save failed", Toast.LENGTH_SHORT).show();
+//                }
+//            } else {
+//                Toast.makeText(ctx, "Save failed", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        // TODO: put this right before mqtt connection is established to retrieve stored username and password AND
+        //  also before initial login. in the first case, it is a background service so it should fail silently.
+        //  in the second case, after app start, the user has to be redirected to login on error
+//        CredentialsClient client = Credentials.getClient(ctx);
+//        CredentialRequest build = new CredentialRequest.Builder()
+//                .setPasswordLoginSupported(true)
+//                .setAccountTypes("thriller")
+//                .build();
+//
+//        client.request(build).addOnCompleteListener(command -> {
+//            if(command.isSuccessful()) {
+//                Credential credential = command.getResult().getCredential();
+//                String accountType = credential.getAccountType();
+//
+//                if (accountType == null) {
+//                    //signInWithPassword(credential.getId(), credential.getPassword());
+//                } else if (accountType.equals("thriller")) {
+//                    // user is already signed in with thriller
+//                }
+//                return;
+//            }
+//
+//            Exception e = command.getException();
+//            Log.e("appcontext", "Could not retrieve user credentials from store: ");
+//            Log.e("appcontext", e.getMessage());
+//            e.printStackTrace();
+//            // redirect to login
+//        });
 
         connOpts.setPassword("changeme".toCharArray());
         connOpts.setUserName("user");
@@ -49,11 +121,23 @@ public class ApplicationContext {
 
         mqttService = new MqttService(
                 MqttClientBuilder.builder()
-                        .broker(MQTT_BROKER_URL)
-                        .clientId(MqttAsyncClient.generateClientId())
+                        .clientId(getUuid(ctx).toString())
                         .build()
                         .build(),
                 connOpts);
+    }
+
+    private UUID getUuid(Context ctx) {
+        UUID clientId;
+        SharedPreferences pref = ctx.getSharedPreferences(ctx.getPackageName(), Context.MODE_PRIVATE);
+        String string = pref.getString("UUID", null);
+        if(string == null){
+            clientId = UUID.randomUUID();
+            pref.edit().putString("UUID", clientId.toString());
+        } else {
+            clientId = UUID.fromString(string);
+        }
+        return clientId;
     }
 
     @Nullable
